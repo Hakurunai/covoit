fa91d1f649c8b084faa4cfd710c1383669d1f173
Guillaume Le Gall <glegall@greta-bretagne-sud.fr>
2026-02-12 00:43:50 +0100

    feat : test a way to inject the enum value into the database for AccountRole

--- src/main/java/fr/greta/cda/s4_covoitmobile/S4CovoitMobileApplication.java
+++ src/main/java/fr/greta/cda/s4_covoitmobile/S4CovoitMobileApplication.java
@@ -1,7 +1,13 @@
 package fr.greta.cda.s4_covoitmobile;
 
+import fr.greta.cda.s4_covoitmobile.data.EAccountRole;
+import fr.greta.cda.s4_covoitmobile.models.AccountRole;
+import fr.greta.cda.s4_covoitmobile.repositories.AccountRoleRepository;
+import fr.greta.cda.s4_covoitmobile.repositories.AccountStatusRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class S4CovoitMobileApplication
@@ -12,4 +18,17 @@ public class S4CovoitMobileApplication
 		SpringApplication.run(S4CovoitMobileApplication.class, args);
 	}
 	
+	
+	@Bean
+	CommandLineRunner initDatabase(AccountRoleRepository roleRepo, AccountStatusRepository statusRepo) {
+		return args -> {
+			if (roleRepo.count() == 0) {
+				for (EAccountRole role : EAccountRole.values()) {
+					AccountRole r = new AccountRole();
+					r.setName(role);
+					roleRepo.save(r);
+				}
+			}
+		};
+	}
 }
