591177085faac3435ad51fae4c1141d7864e4184
Guillaume Le Gall <glegall@greta-bretagne-sud.fr>
2026-03-27 13:25:07 +0100

    feat: add cors configuration

--- src/main/java/fr/greta/cda/s4_covoitmobile/config/SecurityConfig.java
+++ src/main/java/fr/greta/cda/s4_covoitmobile/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -51,11 +52,11 @@ public class SecurityConfig
 		return new BCryptPasswordEncoder();
 	}
 	
-	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http)
 	{
 		http
+			.cors(Customizer.withDefaults())
 			.csrf(AbstractHttpConfigurer::disable) //we made an api, no need for us here, same for session
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(
